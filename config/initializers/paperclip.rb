e6a22bdfb653a8d28ac85eeb51e410dbcf3c4ffd
Erik Katzen <erikkatzen@gmail.com>
2015-04-03 11:57:28 -0500

    I4 done

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+require "paperclip"  
+Paperclip.options[:command_path] = 'C:\Users\IBM_ADMIN\Documents\Web Dev\Rails-Blogger\ImageMagick-6.9.1-0-Q16-x64-static' 
+Paperclip.options[:swallow_stderr] = false
