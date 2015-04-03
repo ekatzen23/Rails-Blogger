3a807440548a9b96fbcd99ef64265f9e85119403
Erik Katzen <erikkatzen@gmail.com>
2015-04-02 23:31:03 -0500

    finished I2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
 	
 	def show
 		@article = Article.find(params[:id])
+		@comment = Comment.new
+		@comment.article_id = @article.id
 	end
 	
 	def new
@@ -35,5 +37,5 @@ class ArticlesController < ApplicationController
 		@article.update(article_params)
 		flash.notice = "Article '#{@article.title}' Updated!"
 		redirect_to article_path(@article)
-		end
+	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+	belongs_to :article
+end
+
+class Article < ActiveRecord::Base
+  has_many :comments
 end
